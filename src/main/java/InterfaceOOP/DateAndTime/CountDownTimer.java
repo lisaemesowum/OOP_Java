ea2d78a81ea249b4e604cee4f3ab1e2bd1550212
Lisa Emesowum Amarachi <lisaemesowum8@gmail.com>
2025-09-07 13:21:55 +0100

    CountDownTimer add

--- src/main/java/InterfaceOOP/DateAndTime/CountDownTimer.java
+++ src/main/java/InterfaceOOP/DateAndTime/CountDownTimer.java
@@ -6,8 +6,9 @@ import java.util.TimerTask;
 
 public class CountDownTimer {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
 
+        Scanner input = new Scanner(System.in);
+//        int totalSeconds = 3600; // 1 hour = 3600 seconds fixed
         //take the input from user
         System.out.println("Enter countdown from: ");
         int user = input.nextInt();  //getting the next int from the user
@@ -16,17 +17,21 @@ public class CountDownTimer {
 
         Timer time = new Timer();
         TimerTask task = new TimerTask() {   //defines what should run repeatedly (printing and counting down).
+//            int count = totalSeconds;
             int count = user;
+
 
             @Override
             //count down with run
             public void run() {
                 //real cooking clock:
-                int minutes = count / 60;
+                int hours = count / 3600;
+//                int minutes = count / 60;
+                int minutes = (count % 3600) / 60;
                 int seconds = count % 60;
 
 //                System.out.println();
-                System.out.printf("%02d:%02d\n", minutes, seconds);   //print minutes and seconds as 2-digit numbers with leading zeros, separated by : and followed by a new line.
+                System.out.printf("%02d:%02d:%02d\n", hours, minutes, seconds);   //print minutes and seconds as 2-digit numbers with leading zeros, separated by : and followed by a new line.
                 count--; //Decreases the counter.
                 //using the if method to check if count is less then 0
                 if(count < 0){  //If the count is below 0, stops the timer.
